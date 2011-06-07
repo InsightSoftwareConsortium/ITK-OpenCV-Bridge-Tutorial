920eff021bc9b917423fd48928fdb3e3bc36c920
Matt Leotta <matt.leotta@kitware.com>
2011-06-07 16:46:06 -0400

    Implemented Answer to OpenCV Exercise 1: Basic Filtering.
    Median filtering is replace by Canny edge detection.  Canny edge
    detection also requires that images are converted from color to gray.

--- Exercises/OpenCVIntroduction/exercise1/BasicFilteringOpenCVAnswer.cxx
+++ Exercises/OpenCVIntroduction/exercise1/BasicFilteringOpenCVAnswer.cxx
@@ -16,10 +16,53 @@
  *
  *=========================================================================*/
 
+#include <cv.h>
+#include <highgui.h>
+
+#include <iostream>
+#include <string>
+
+
 int main ( int argc, char **argv )
 {
-  // Here Replace the Median filter from the previous example
-  // with the CannyEdge detection filter
+  // if there are no arguments then print usage and exit
+  // Note: argv[0] is always set to the executable name
+  if( argc < 2 )
+  {
+    std::cout << "Usage: "<< argv[0] <<" input_image output_image"<<std::endl;
+    return -1;
+  }
+
+  // read the input image from the specified file
+  cv::Mat inputImage = cv::imread( argv[1] );
+  // create a matrix object to store the grayscale image
+  cv::Mat grayImage;
+  // create a matrix object to store the resulting image
+  cv::Mat resultImage;
+  // Convert the color image to grayscale because Canny requires grayscale
+  cv::cvtColor(inputImage, grayImage, CV_BGR2GRAY);
+  // apply Canny edge detection to the image
+  cv::Canny( grayImage, resultImage, 128, 255 );
+
+  // if there is only an input file specified then display the result on screen
+  if(argc < 3)
+  {
+    // The title string for the GUI window
+    std::string windowName = "Exercise 1: Basic Filtering in OpenCV";
+    // create a window, CV_WINDOW_FREERATIO is needed for display with OpenGL
+    cv::namedWindow( windowName, CV_WINDOW_FREERATIO);
+    // show the image in the previously created window
+    cv::imshow( windowName, resultImage );
+    // wait for the user to press a key, then continue
+    cv::waitKey();
+  }
+  // otherwise, write the resulting image to the specified output file.
+  else
+  {
+    // write the resulting image to a file
+    cv::imwrite( argv[2], resultImage );
+  }
+
   return 0;
 }
 
