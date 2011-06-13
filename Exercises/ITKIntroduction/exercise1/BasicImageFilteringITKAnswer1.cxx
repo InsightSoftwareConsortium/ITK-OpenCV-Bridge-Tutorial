a8f81f875593680b19583a06cfdece4855d708cf
Luis Ibanez <luis.ibanez@kitware.com>
2011-06-13 16:00:37 -0400

    ENH: Added Answer to ITK exercise1.
    And started instructions on how to build all the tree.

---
+++ Exercises/ITKIntroduction/exercise1/BasicImageFilteringITKAnswer1.cxx
@@ -0,0 +1,75 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkMeanImageFilter.h"
+
+int main( int argc, char * argv [] )
+{
+  if( argc < 3 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputImageFile   outputImageFile" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef  unsigned char  InputPixelType;
+  typedef  unsigned char  OutputPixelType;
+
+  const unsigned int Dimension = 2;
+
+  typedef itk::Image< InputPixelType,  Dimension >   InputImageType;
+  typedef itk::Image< OutputPixelType, Dimension >   OutputImageType;
+
+  typedef itk::ImageFileReader< InputImageType  >  ReaderType;
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( argv[1] );
+  writer->SetFileName( argv[2] );
+
+  typedef itk::MeanImageFilter< InputImageType, OutputImageType > FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+
+  InputImageType::SizeType indexRadius;
+
+  indexRadius[0] = 1; // radius along x
+  indexRadius[1] = 1; // radius along y
+
+  filter->SetRadius( indexRadius );
+
+  filter->SetInput( reader->GetOutput() );
+  writer->SetInput( filter->GetOutput() );
+
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
+
