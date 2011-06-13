f68a83c80cc2b4c03f44398f76495a229567a510
Matt Leotta <matt.leotta@kitware.com>
2011-06-13 13:32:52 -0400

    Updated include paths in OpenCV exercise 2.

--- Exercises/OpenCVIntroduction/exercise2/BasicVideoFilteringOpenCVAnswer.cxx
+++ Exercises/OpenCVIntroduction/exercise2/BasicVideoFilteringOpenCVAnswer.cxx
@@ -16,8 +16,8 @@
  *
  *=========================================================================*/
 
-#include <cv.h>
-#include <highgui.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <iostream>
 #include <string>
