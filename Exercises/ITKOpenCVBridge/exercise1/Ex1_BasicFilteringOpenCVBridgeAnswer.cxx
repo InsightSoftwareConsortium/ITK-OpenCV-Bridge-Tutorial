afbbdb986d379be5b5e8ef961a1ab4a0ebd24c25
Patrick Reynolds <patrick.reynolds@kitware.com>
2011-06-14 15:57:56 -0400

    ENH: Adding try-catch on filter updates and updating the presentation.

--- Exercises/ITKOpenCVBridge/exercise1/Ex1_BasicFilteringOpenCVBridgeAnswer.cxx
+++ Exercises/ITKOpenCVBridge/exercise1/Ex1_BasicFilteringOpenCVBridgeAnswer.cxx
@@ -51,7 +51,15 @@ int main ( int argc, char **argv )
   filter->SetNumberOfIterations( 20 );
 
   filter->SetInput( itkImage );
-  filter->Update();
+  try
+    {
+    filter->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
 
   cv::Mat resultImage =
     BridgeType::ITKImageToCVMat< OutputImageType >( filter->GetOutput() );
@@ -60,7 +68,10 @@ int main ( int argc, char **argv )
   {
     std::string windowName = "Exercise 1: Basic Filtering in OpenCV & ITK";
     cv::namedWindow( windowName, CV_WINDOW_FREERATIO);
-    cv::imshow( windowName, resultImage );
+    cvResizeWindow( windowName.c_str(), resultImage.cols, resultImage.rows+50 );
+    cv::Mat scaled;
+    resultImage.convertTo(scaled, CV_8UC1 );
+    cv::imshow( windowName, scaled );
     cv::waitKey();
   }
   else
